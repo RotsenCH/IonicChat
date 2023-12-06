45725b320d1db74712c5d3fdf6363c7f9f783416
RotsenCH <117753370+RotsenCH@users.noreply.github.com>
2023-12-06 16:49:12 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyAx1rhCDuGz0G7x5hngipWbm10_XYFlbwA",
-    authDomain: "bnch-9456f.firebaseapp.com",
-    projectId: "bnch-9456f",
-    storageBucket: "bnch-9456f.appspot.com",
-    messagingSenderId: "499402471592",
-    appId: "1:499402471592:web:c1812632203f748acf2a9d",
-    measurementId: "G-WD7306H8JM",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
 }
 }
 
